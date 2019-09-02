44c98b234e9e4320e3615977ecefdfdcfa57caa0
Sylvain MIRA <smira@e3r11p23.42.fr>
2019-09-02 16:14:59 +0200

    C00

--- C00/ex02/ft_print_reverse_alphabet.c
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smira <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/29 17:41:55 by smira             #+#    #+#             */
-/*   Updated: 2019/08/29 19:02:19 by smira            ###   ########.fr       */
+/*   Created: 2019/09/02 16:00:15 by smira             #+#    #+#             */
+/*   Updated: 2019/09/02 16:04:09 by smira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
 	char	ch;
 
