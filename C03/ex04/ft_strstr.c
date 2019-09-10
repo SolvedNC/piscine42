9bdfa3ddb6bbe4c3438d47c5e2433ff9375df96e
Sylvain MIRA <smira@e3r13p8.42.fr>
2019-09-10 18:06:16 +0200

    clean

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smira <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/05 15:26:48 by smira             #+#    #+#             */
+/*   Updated: 2019/09/06 15:33:05 by smira            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char		*ft_strstr(char *str, char *to_find)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	size;
+
+	i = 0;
+	j = 0;
+	size = 0;
+	while (to_find[size])
+		size++;
+	if (size == 0)
+		return (str);
+	while (str[i])
+	{
+		while (str[i + j] == to_find[j])
+		{
+			if (j == size - 1)
+				return (str + i);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
